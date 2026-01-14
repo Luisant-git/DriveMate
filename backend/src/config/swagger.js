e8cb02868005203a6908e249ea474fb5ea148421
ragul.se21 <ragul.se21@bitsathy.ac.in>
2026-01-14 17:29:52 +0530

    api url fix

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -10,7 +10,11 @@ const options = {
     },
     servers: [
       {
-        url: process.env.API_URL || `http://localhost:${process.env.PORT || 5000}`,
+        url: 'https://drivemate.api.luisant.cloud',
+        description: 'Production server',
+      },
+      {
+        url: `http://localhost:${process.env.PORT || 5000}`,
         description: 'Development server',
       },
     ],
