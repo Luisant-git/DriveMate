62c653cbfc96fafe1c1cfe55dd2a0e775d4e9855
naveenv0906 <naveenv0906@gmail.com>
2026-03-12 13:26:53 +0530

    put copyright symbol

--- frontend/pages/LandingPage.tsx
+++ frontend/pages/LandingPage.tsx
@@ -369,7 +369,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onGetStarted }) => {
                     </div>
                     <div className="pt-8 border-t border-gray-900 text-[10px] text-gray-300 font-bold uppercase tracking-widest flex flex-col md:flex-row justify-between items-center gap-4">
                         <p> This is an official platform of SNPNBC Consultant and Manpower Services, <br />
-owned by Kathariguppe Gangadharappa Chandrappa.</p>
+© owned by Kathariguppe Gangadharappa Chandrappa.</p>
                         <div className="flex gap-4">
                             <a href="https://www.facebook.com/share/18P8cPUbxU/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">
                                 <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
